2aaba5aca3a2dc4e22d65f1fbea83f7b4eda0505
Abishek4141 <abishekgold4141@gmail.com>
2026-07-21 16:06:53 +0530

    Replace linked list with new string data and update display
    Replaced original linked list with a new list of strings and updated the display logic to print the new list after deleting the head.

--- del_beg.py
+++ del_beg.py
@@ -1,25 +1,30 @@
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-def display(head):
-    temp = head
-    while temp is not None:
-        print(temp.data,end="->")
-        temp = temp.next
-    print("None")
-n1 = Node(5)
-n2 = Node(10)
-n3 = Node(20)
-n4 = Node(30)
-n5 = Node(40)
+     def __init__(self,data):
+         self.data=data
+         self.next=None
+n1=Node("Nira")
+n2=Node("My name is billa")
+n3=Node("Mangatha da")
+n4=Node("king of the sea")
+n5=Node("neeyum naanum anbe")
+n6=Node("see you again")
+n7=Node("Va asura vaa")
+n8=Node("vaadi pulla vaadi")
+n9=Node("Manamaganin Sathiyam")
+n10=Node("unnodu sernthu")
 n1.next=n2
 n2.next=n3
 n3.next=n4
 n4.next=n5
-print("Orginal Linked List")
+n5.next=n6
+n6.next=n7
+n7.next=n8
+n8.next=n9
+n9.next=n10
 head=n1
-display(head)
-print("\n Deletion at beginning")
 head=head.next
-display(head)
+temp=head
+while temp is not None:
+    print(temp.data)
+    temp=temp.next
+print("None")
