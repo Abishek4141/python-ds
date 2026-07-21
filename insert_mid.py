e19bc0dd586c0b076caf1164cffefe7e23c6da0f
Abishek4141 <abishekgold4141@gmail.com>
2026-07-21 16:09:09 +0530

    Update insert_mid.py

--- insert_mid.py
+++ insert_mid.py
@@ -8,13 +8,25 @@ def display(head):
         print(temp.data,end="->")
         temp = temp.next
     print("None")
-n1 = Node(5)
-n2 = Node(10)
-n3 = Node(20)
-n4 = Node(30)
+n1=Node("Nira")
+n2=Node("My name is billa")
+n3=Node("Mangatha da")
+n4=Node("king of the sea")
+n5=Node("neeyum naanum anbe")
+n6=Node("see you again")
+n7=Node("Va asura vaa")
+n8=Node("vaadi pulla vaadi")
+n9=Node("Manamaganin Sathiyam")
+n10=Node("unnodu sernthu")
 n1.next=n2
 n2.next=n3
 n3.next=n4
+n4.next=n5
+n5.next=n6
+n6.next=n7
+n7.next=n8
+n8.next=n9
+n9.next=n10
 print("Orginal Linked List")
 head=n1
 display(head)
